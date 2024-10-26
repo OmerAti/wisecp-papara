16dbde48e12a801e5443f2a0c09fdd41f1389ff4
Ömer ATABER <67821076+OmerAti@users.noreply.github.com>
2024-10-26 15:51:52 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 return [
     'meta'     => [
         'name'    => 'Papara',
-        'version' => '1.0',
+        'version' => '1.2',
         'logo'    => 'logo.png',
     ],
     'settings' => [
